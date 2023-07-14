a41727fe02866a0ee386f4e80dc0edd4fe8497db
Esteban Mendez <estebanmendez@Estebans-MacBook-Pro.local>
2023-07-14 12:29:15 -0400

    rendered events data

--- script.js
+++ script.js
@@ -12,52 +12,52 @@ function getTopHeadlines() {
       //   title = response.data.articles.title;
       //   imgUrl = response.data.articles.urlToImage;
 
-      //renderCarousel(response.data.articles);
+      renderCarousel(response.data._embedded.events);
     })
     .catch((error) => {
       console.log(error);
     });
 }
 
-// let slideIndex = 1;
-// // showSlides(slideIndex);
+let slideIndex = 1;
+showSlides(slideIndex);
 
-// function plusSlides(n) {
-//   //console.log("what is n:", n);
-//   showSlides((slideIndex += n));
-// }
+function plusSlides(n) {
+  //console.log("what is n:", n);
+  showSlides((slideIndex += n));
+}
 
-// function showSlides(n) {
-//   //   console.log("Line 32, n is: ", n)
-//   let i;
-//   let slides = document.getElementsByClassName("slides");
-//   //   console.log("slides.length: ", slides.length)
-//   if (n > slides.length) {
-//     // if index greater than slides.length (aka 21 > 20) then index goes back to 1
-//     slideIndex = 1;
-//     console.log("new  ", slideIndex);
-//   }
-//   if (n < 1) {
-//     slideIndex = slides.length;
-//     console.log("AAHH ", slideIndex);
-//   }
-//   for (i = 0; i < slides.length; i++) {
-//     console.log("slides [i]: ", slides[i]);
-//     slides[i].style.display = "none";
-//     slides[slideIndex - 1].style.display = "block";
-//   }
-// }
+function showSlides(n) {
+  //   console.log("Line 32, n is: ", n)
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  //   console.log("slides.length: ", slides.length)
+  if (n > slides.length) {
+    // if index greater than slides.length (aka 21 > 20) then index goes back to 1
+    slideIndex = 1;
+    // console.log("new  ", slideIndex);
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+    // console.log("AAHH ", slideIndex);
+  }
+  for (i = 0; i < slides.length; i++) {
+    // console.log("slides [i]: ", slides[i]);
+    slides[i].style.display = "none";
+    slides[slideIndex - 1].style.display = "block";
+  }
+}
 
-// function renderCarousel(articles) {
-//   for (let i = 0; i < articles.length; i++) {
-//     $(".carousel").append(`
-//     <div class="slides fade">
-//         <img src=${articles[i].urlToImage} alt="article">
-//         <div class="title-text">${articles[i].title}</div>
-//     </div>
-//     `);
-//   }
-//   showSlides(slideIndex);
-// }
+function renderCarousel(events) {
+  for (let i = 0; i < events.length; i++) {
+    $(".carousel").append(`
+    <div class="slides fade">
+        <img src=${events[i].images[0].url} alt="article">
+        <div class="title-text">${events[i].name.toUpperCase()}</div>
+    </div>
+    `);
+  }
+  showSlides(slideIndex);
+}
 
  getTopHeadlines();
