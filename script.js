a5e83ac4a8992f7de915c6d4f6d6a57e7860b624
Esteban Mendez <estebanmendez@Estebans-MacBook-Pro.local>
2023-07-14 16:35:37 -0400

    edited home page, added form, input, edited positioning

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let apiKey = `0DsuAyGvECXLVAGtuUju6HSU98Eig6H3`;
 let title;
 let imgUrl;
 
-function getTopHeadlines() {
+function getResults() {
   axios
     .get(
-      `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}`
+      `https://app.ticketmaster.com/discovery/v2/events.json?attractionId=K8vZ917Gku7&countryCode=US&apikey=${apiKey}`
     )
     .then((response) => {
       console.log(response);
@@ -18,46 +18,56 @@ function getTopHeadlines() {
       console.log(error);
     });
 }
+//getResults();
 
-let slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-  //console.log("what is n:", n);
-  showSlides((slideIndex += n));
-}
 
-function showSlides(n) {
-  //   console.log("Line 32, n is: ", n)
-  let i;
-  let slides = document.getElementsByClassName("slides");
-  //   console.log("slides.length: ", slides.length)
-  if (n > slides.length) {
-    // if index greater than slides.length (aka 21 > 20) then index goes back to 1
-    slideIndex = 1;
-    // console.log("new  ", slideIndex);
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-    // console.log("AAHH ", slideIndex);
-  }
-  for (i = 0; i < slides.length; i++) {
-    // console.log("slides [i]: ", slides[i]);
-    slides[i].style.display = "none";
-    slides[slideIndex - 1].style.display = "block";
-  }
-}
 
-function renderCarousel(events) {
-  for (let i = 0; i < events.length; i++) {
-    $(".carousel").append(`
-    <div class="slides fade">
-        <img src=${events[i].images[0].url} alt="article">
-        <div class="title-text">${events[i].name.toUpperCase()}</div>
-    </div>
-    `);
-  }
-  showSlides(slideIndex);
-}
 
- getTopHeadlines();
+
+
+
+
+// ===================== CAROUSEL ==============================
+// let slideIndex = 1;
+// showSlides(slideIndex);
+
+// function plusSlides(n) {
+//   //console.log("what is n:", n);
+//   showSlides((slideIndex += n));
+// }
+
+// function showSlides(n) {
+//   //   console.log("Line 32, n is: ", n)
+//   let i;
+//   let slides = document.getElementsByClassName("slides");
+//   //   console.log("slides.length: ", slides.length)
+//   if (n > slides.length) {
+//     // if index greater than slides.length (aka 21 > 20) then index goes back to 1
+//     slideIndex = 1;
+//     // console.log("new  ", slideIndex);
+//   }
+//   if (n < 1) {
+//     slideIndex = slides.length;
+//     // console.log("AAHH ", slideIndex);
+//   }
+//   for (i = 0; i < slides.length; i++) {
+//     // console.log("slides [i]: ", slides[i]);
+//     slides[i].style.display = "none";
+//     slides[slideIndex - 1].style.display = "block";
+//   }
+// }
+
+// function renderCarousel(events) {
+//   for (let i = 0; i < events.length; i++) {
+//     $(".carousel").append(`
+//     <div class="slides fade">
+//         <img src=${events[i].images[0].url} alt="article">
+//         <div class="title-text">${events[i].name.toUpperCase()}</div>
+//     </div>
+//     `);
+//   }
+//   showSlides(slideIndex);
+// }
+
+ 
